85cdd1ca5de7f84fb5833f9d39a3a42b0f5e48bb
cong.pi <cong.pi@swiftpass.cn>
2025-08-13 10:08:58 +0800

    remove option require

--- init.lua
+++ init.lua
@@ -200,7 +200,7 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 require 'custom.config.remaps'
-require 'custom.config.option'
+-- require 'custom.config.option'
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
